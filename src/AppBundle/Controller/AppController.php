267c5aa70c61f36e41d271805470f78abde45eb6
Amos Garner <aagarner.7@gmail.com>
2017-03-07 15:57:27 -0500

    Add routing and renaming to AppBundle.

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class AppController extends Controller
 {
     public function indexAction()
     {
